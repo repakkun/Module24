68f56b53f04de6e91b59f8252362d29652cd52a1
rePakkun <cj.ilya@yandex.ru>
2021-03-06 19:27:49 +0300

    Добавьте файлы проекта.

---
+++ Module24/CalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module24
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Addition(4, 2) == 6);
+        }
+        [Test]
+        public void SubstractionMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Subtraction(4, 2) == 2);
+        }
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Miltiplication(4, 2) == 8);
+        }
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Division(8, 2) == 4);
+        }
+    }
+}
